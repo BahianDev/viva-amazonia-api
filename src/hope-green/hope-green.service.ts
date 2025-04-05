5f5a9562728c5a5fc25db1175634990526b2c296
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2025-04-05 01:55:37 -0300

    fix

--- src/hope-green/hope-green.service.ts
+++ src/hope-green/hope-green.service.ts
@@ -18,27 +18,29 @@ export class HopeGreenService {
       MARKETPLACE_ABI,
       provider,
     );
-  
+
     const listings = await contract.getActiveListings();
-  
+
     const enrichedListings = await Promise.all(
       listings.map(async (listing: any) => {
         // Converte o tokenId diretamente para string
-        const tokenId = String(listing.tokenId);
+        const tokenId = Number(listing.tokenId);
+        const price = Number(listing.price);
         const metadataUrl = `https://hope-green.s3.us-east-2.amazonaws.com/metadata/${tokenId}.json`;
         let metadata = null;
         try {
           const response = await fetch(metadataUrl);
           metadata = await response.json();
         } catch (error) {
-          console.error(`Erro ao buscar metadata para tokenId ${tokenId}:`, error);
+          console.error(
+            `Erro ao buscar metadata para tokenId ${tokenId}:`,
+            error,
+          );
         }
-        return { ...listing, tokenId, metadata };
-      })
+        return { ...listing, price, tokenId, metadata };
+      }),
     );
-  
+
     return enrichedListings;
   }
-  
-  
 }
